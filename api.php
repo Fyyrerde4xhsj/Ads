facf9c2dfcb1581465d1139aa380471e923a98be
Fyyrerde4xhsj <ankurboro236@gmail.com>
2025-10-31 09:49:37 +0530

    Delete api.php
    R class

--- api.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Http\Controllers\Api\LinkController as ApiLinkController;
-
-Route::middleware(['auth:api'])->group(function () {
-    Route::post('/shorten', [ApiLinkController::class, 'shorten']);
-    Route::get('/links', [ApiLinkController::class, 'index']);
-    Route::get('/statistics', [ApiLinkController::class, 'statistics']);
-});
